59564d2a9af3c5c6bb8003046e1c9ff43fbe6376
Marteen21 <znmarteen21@gmail.com>
2016-01-10 22:11:11 +0100

    v0.2

--- WowMemoryReaderBM/Constants/Offsets.cs
+++ WowMemoryReaderBM/Constants/Offsets.cs
@@ -22,5 +22,57 @@ namespace WowMemoryReaderBM.Constants {
             TargetGuid64 = 0x40,
 
         }
+        internal enum Globals : uint {
+            PlayerName = 0x9BE820,
+            CurrentRealm = 0x9BE9AE,
+            CurrentTargetGUID = 0xAD7448,
+            LastTargetGUID = 0xAD7450,
+            FocusTargetGUID = 0xAD7468,
+            MouseOverGUID = 0xAD7438,
+            PetGUID = 0xB43B60,
+            FollowGUID = 0x9D61D8,
+            ComboPoint = 0xAD74F1,
+            LootWindow = 0xB45230,
+            Timestamp = 0x9C0C7C,
+            BuildNumber = 0xAB4214,
+            GetMinimapZoneText = 0xAD7414,
+            GetZoneText = 0xAD741C,
+            GetSubZoneText = 0xAD7418,
+            GetZoneID = 0xAD74B0,
+            IsInGame = 0xAD7426,
+            ContinentID = 0x8A2710,
+            LastErrorMessage = 0xAD6828,
+            IsLoadingOrConnecting = 0xABB9AC,
+            GetCurrencyInfo = 0x914F48,
+            GetHomeBindAreaId = 0x9D4D7C,
+            PetSpellBookNumSpells = 0xB33CA4,
+            PetSpellBookNumSpellsPtr = 0xB33CA8,
+            SpellIsTargetting = 0xACD654,
+            SpellIsPending = 0xACD770,
+            ScriptGetLocale = 0x9732FC,
+            CursorType = 0x93D250,
+            MirrorTimer = 0xAD78D0,
+            GetNumInstalledAddons = 0x93A74C,
+            BaseAddons = 0x93A750,
+            TotalGuildMembers = 0xB35ECC,
+            GuildRosterInfoBase = 0xB35F64
+        }
+        public static void PrintGlobalstoConsole() {
+            foreach (Offsets.Globals enumValue in Enum.GetValues(typeof(Offsets.Globals))) {
+                try {
+                    if (enumValue.ToString().Contains("GUID")) {
+                        Console.WriteLine(enumValue.ToString().PadRight(30) + String.Format("0x{0:X}", Program.wow.ReadUInt64((uint)Program.wow.MainModule.BaseAddress + (uint)enumValue)).PadRight(30) + "0x{0:X}", (uint)Program.wow.MainModule.BaseAddress + enumValue);
+                    }
+                    else if(enumValue.ToString().Contains("Name") || enumValue.ToString().Contains("Text")){
+                        Console.WriteLine(enumValue.ToString().PadRight(30) + Program.wow.ReadASCIIString(((uint)Program.wow.MainModule.BaseAddress + (uint)enumValue),64).PadRight(30) + "0x{0:X}", (uint)Program.wow.MainModule.BaseAddress + enumValue);
+                    }
+                    else {
+                        Console.WriteLine(enumValue.ToString().PadRight(30) + (Program.wow.ReadUInt((uint)Program.wow.MainModule.BaseAddress + (uint)enumValue)).ToString().PadRight(30) + "0x{0:X}", (uint)Program.wow.MainModule.BaseAddress + enumValue);
+                    }
+
+                }
+                catch { }
+            }
+        }
     }
 }
--- WowMemoryReaderBM/Program.cs
+++ WowMemoryReaderBM/Program.cs
@@ -12,23 +12,27 @@ namespace WowMemoryReaderBM {
         public static BlackMagic wow;
         public static GameObject FirstObject;
         public static GameObject PlayerObject;
+        private static uint ObjMgr;
+        
 
         static void Main(string[] args) {
             wow = new BlackMagic();
             wow.OpenProcessAndThread(SProcess.GetProcessFromWindowTitle(PROCESS_WINDOW_TITLE));
             PlayerObject = new GameObject(wow.MainModule.BaseAddress, new uint[] { 0x00A42788, 0x9C, 0x5C });
-            FirstObject = new GameObject(wow.MainModule.BaseAddress, new uint[] { (uint)Offsets.ObjectManager.CurMgrPointer, (uint)Offsets.ObjectManager.CurMgrOffset, (uint)Offsets.ObjectManager.FirstObject });
-            FirstObject.BaseAddress += 0x10;
+            GameObject Player2Object = new GameObject(wow.MainModule.BaseAddress, new uint[] { 0x00a70c50, 0x38, 0x24 });
+            ObjMgr = wow.ReadUInt(wow.ReadUInt((uint)wow.MainModule.BaseAddress + (uint)Offsets.ObjectManager.CurMgrPointer)+ (uint)Offsets.ObjectManager.CurMgrOffset);
+            FirstObject = new GameObject();
+            FirstObject.BaseAddress = wow.ReadUInt(ObjMgr + (uint)Offsets.ObjectManager.FirstObject);
             PlayerObject.PrintDescriptorsToConsol();
+            Player2Object.PrintDescriptorsToConsol();
             FirstObject.PrintDescriptorsToConsol();
-            Console.WriteLine(FirstObject.BaseAddress);
-            Console.WriteLine("0x{0:X}",PlayerObject.Targetguid);
-            Console.WriteLine("0x{0:X}", Masker.Guid2UnitType(PlayerObject.Targetguid));
+
             Console.WriteLine(Masker.Guid2UnitID(PlayerObject.Targetguid));
             GameObject TargetObject = new GameObject();
             TargetObject.Guid = PlayerObject.Targetguid;
             TargetObject.SetObjectBaseByGuid();
             TargetObject.PrintDescriptorsToConsol();
+            Offsets.PrintGlobalstoConsole();
             Console.ReadLine();
         }
         public static void mask(UInt64 a,UInt64 b) {
