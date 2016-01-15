825e3f791f1a32601fc44895282909165003b335
Tormasan <jackcrever@gmail.com>
2016-01-16 00:52:37 +0100

    csudu mudu 1.0v

--- WowMemoryReaderBM/Bots/DruidDPS.cs
+++ WowMemoryReaderBM/Bots/DruidDPS.cs
@@ -7,81 +7,105 @@ using WowMemoryReaderBM.Constants;
 using WowMemoryReaderBM.Models;
 using WowMemoryReaderBM.Objects;
 
-namespace WowMemoryReaderBM.Bots {
-    class DruidDPS {
+namespace WowMemoryReaderBM.Bots
+{
+    class DruidDPS
+    {
 
-        public static DoT rake = new DoT(1822, Const.WindowsVirtualKey.K_2);
-        public static DoT rip = new DoT(1079, Const.WindowsVirtualKey.K_R);
-        public static DoT mangle = new DoT(33917, Const.WindowsVirtualKey.K_5);
-        public static DoT FF = new DoT(770, Const.WindowsVirtualKey.K_4);
+        private static DoT rake = new DoT(1822, Const.WindowsVirtualKey.K_2);
+        private static DoT rip = new DoT(1079, Const.WindowsVirtualKey.K_R);
+        private static DoT mangle = new DoT(33876, Const.WindowsVirtualKey.K_5);
+        private static DoT FF = new DoT(91565, Const.WindowsVirtualKey.K_4);
+        private static Spell catform = new Spell(768);
 
-
-
-        //    FFBuff="Faerie Fire(Feral)";
-        //    FFKey=4;
-
-        //    chargeKey=0;
-
-
-
-        //    // range-ben van e a target 
-        //    // a hátát látom e a targetnak 
-        //    if(targetEnemy==1){
-        //        if(FFBuff=0 && range<30)
-        //        {
-        //            SendKey(FFkey);
-        //        }
-
-        //        if (range>8 && range<25)
-        //        {
-        //            chargeKey
-        //        }
-
-        //        if(range<melee)
-        //        {
-        //            if(mangleBuff==0)
-        //            {
-        //                mangleKey
-        //            }
-        //            if(rakeBuff==0)
-        //            {
-        //                rakeKey
-        //            }
-        //            if(combo=5 && ripBuff==0)
-        //            {
-        //                ripKey
-        //            }
-        //            if()           
-
-        //        }   
-
-
-        //}
-        public static void DpsEvent(Object source, System.Timers.ElapsedEventArgs e) {
+       public static void DpsEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
             UInt64 CurrTargetGUID = Program.wow.ReadUInt64((uint)Program.wow.MainModule.BaseAddress + (uint)Constants.Const.Globals.CurrentTargetGUID);
             Program.PlayerObject.Refresh();
-            if (Program.PlayerObject.Healthpercent >= 80 && Program.PlayerObject.Manapercent <= 50) {
-                WarlockDPS.LifeTap.SendCast();
-            }
-            if (CurrTargetGUID != 0) {
+            if (CurrTargetGUID != 0)
+            {
                 Program.TargetObject = new GameObject(CurrTargetGUID);
                 Program.TargetObject.RefreshBuffIDs();
-                if (!WarlockDPS.Corruption.ReCast(Program.TargetObject) && !WarlockDPS.BaneofAgony.ReCast(Program.TargetObject) && !WarlockDPS.ShadowTrance.IfCast(Program.PlayerObject)) {
-                    if (!Program.PlayerObject.IsMoving) {
-                        if (!WarlockDPS.UnstableAffliction.ReCast(Program.TargetObject) && !WarlockDPS.Haunt.ReCast(Program.TargetObject)) {
-                            WarlockDPS.DrainLife.ReCast(Program.TargetObject);
-                        }
-                    }
-                    else {
-                        if (!WarlockDPS.CurseoftheElements.ReCast(Program.TargetObject)) {
-                            WarlockDPS.FellFlame.SendCast();
-                        }
-                    }
 
+
+                if (Program.PlayerObject.HasBuff(catform.ID))
+                {
+                    DruidDPS.rake.ReCast(Program.TargetObject);
+                    DruidDPS.mangle.ReCast(Program.TargetObject);
+                    DruidDPS.FF.ReCast(Program.TargetObject);
+                    DruidDPS.rip.ReCast(Program.TargetObject);
                 }
-            }
+
+            }         
+      
         }
+
     }
 }
+
+
+//    FFBuff="Faerie Fire(Feral)";
+//    FFKey=4;
+
+//    chargeKey=0;
+
+
+
+//    // range-ben van e a target 
+//    // a hátát látom e a targetnak 
+//    if(targetEnemy==1){
+//        if(FFBuff=0 && range<30)
+//        {
+//            SendKey(FFkey);
+//        }
+
+//        if (range>8 && range<25)
+//        {
+//            chargeKey
+//        }
+
+//        if(range<melee)
+//        {
+//            if(mangleBuff==0)
+//            {
+//                mangleKey
+//            }
+//            if(rakeBuff==0)
+//            {
+//                rakeKey
+//            }
+//            if(combo=5 && ripBuff==0)
+//            {
+//                ripKey
+//            }
+//            if()           
+
+//        }   
+
+
+//}
+
+//if (Program.PlayerObject.Healthpercent >= 80 && Program.PlayerObject.Manapercent <= 50) {
+//    WarlockDPS.LifeTap.SendCast();
+//}
+//if (CurrTargetGUID != 0) {
+//    Program.TargetObject = new GameObject(CurrTargetGUID);
+//    Program.TargetObject.RefreshBuffIDs();
+//    if (!WarlockDPS.Corruption.ReCast(Program.TargetObject) && !WarlockDPS.BaneofAgony.ReCast(Program.TargetObject) && !WarlockDPS.ShadowTrance.IfCast(Program.PlayerObject)) {
+//        if (!Program.PlayerObject.IsMoving) {
+//            if (!WarlockDPS.UnstableAffliction.ReCast(Program.TargetObject) && !WarlockDPS.Haunt.ReCast(Program.TargetObject)) {
+//                WarlockDPS.DrainLife.ReCast(Program.TargetObject);
+//            }
+//        }
+//        else {
+//            if (!WarlockDPS.CurseoftheElements.ReCast(Program.TargetObject)) {
+//                WarlockDPS.FellFlame.SendCast();
+//            }
+//        }
+
+//    }
+//}
+
+
 
 
