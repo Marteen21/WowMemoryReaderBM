b99730b3ff3215a3b28327a87f6992f28080ee05
Tormasan <jackcrever@gmail.com>
2016-01-15 23:32:42 +0100

    dudu dps

--- WowMemoryReaderBM/Bots/DruidDPS.cs
+++ WowMemoryReaderBM/Bots/DruidDPS.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WowMemoryReaderBM.Constants;
+using WowMemoryReaderBM.Models;
 
 
 
 namespace WowMemoryReaderBM.Bots {
     class DruidDPS {
+
+        public static DoT rake = new DoT(1822, Const.WindowsVirtualKey.K_2);
+        public static DoT rip = new DoT(1079, Const.WindowsVirtualKey.K_R);
+        public static DoT mangle = new DoT(33917, Const.WindowsVirtualKey.K_5);
+        public static DoT FF = new DoT(770, Const.WindowsVirtualKey.K_4);
+
+
 
         //    FFBuff="Faerie Fire(Feral)";
         //    FFKey=4;
